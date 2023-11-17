86cb24720076ed6b29b61721cf79703f2f73bc95
karthik-esper <kgangireddy04@gmail.com>
2023-11-17 14:13:24 -0500

    enums should be done

--- src/main/java/com/rupizza/Topping.java
+++ src/main/java/com/rupizza/Topping.java
@@ -1,5 +1,26 @@
 package com.rupizza;
 
 public enum Topping {
+    SAU("Sausage"),
+    PE("Pepperoni"),
+    BE("Beef"),
+    HA("Ham"),
+    SH("Shrimp"),
+    SQ("Squid"),
+    CM("Crab Meat"),
+    GP("Green Pepper"),
+    ON("Onion"),
+    MU("Mushroom"),
+    BO("Black Olive"),
+    PI("Pineapple"),
+    JA("Jalapeno");
 
+    private final String toppingName;
+
+    private Topping (String toppingName) {
+        this.toppingName = toppingName;
+    }
+    public String getToppingName () {
+        return this.toppingName;
+    }
 }
