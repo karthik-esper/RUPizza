993047b7ee8fb608a4cdaddcfaad0f6b59a5d33e
karthik-esper <kgangireddy04@gmail.com>
2023-11-16 22:55:43 -0500

    stuff!

--- src/main/java/com/rupizza/HelloController.java
+++ src/main/java/com/rupizza/HelloController.java
@@ -7,8 +7,28 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void createSpecialty () {
+        System.out.println("Button got clicked");
+    }
+    @FXML
+    protected void createCustom () {
+        System.out.println("Button got clicked");
+    }
+
+    @FXML
+    protected void viewCurrentOrder () {
+        System.out.println("Button got clicked");
+    }
+
+    @FXML
+    protected void viewOrderHistory () {
+        System.out.println("Button got clicked");
     }
 }
