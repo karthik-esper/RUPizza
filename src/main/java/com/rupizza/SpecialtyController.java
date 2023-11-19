e2bbc4e779c134ad633987d8ee03641da3236234
karthik-esper <kgangireddy04@gmail.com>
2023-11-18 19:16:06 -0500

    added a combobox to specialty

--- src/main/java/com/rupizza/SpecialtyController.java
+++ src/main/java/com/rupizza/SpecialtyController.java
@@ -3,10 +3,24 @@ package com.rupizza;
 import javafx.collections.ObservableList;
 import javafx.beans.Observable;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.collections.FXCollections;
 
 public class SpecialtyController {
     @FXML
     private ListView<Topping> specialtyToppings;
     private ObservableList<Topping> specialtyItems;
+    @FXML
+    private ComboBox<String> specialtyChoices;
+
+    @FXML
+    public void initialize() {
+        specialtyChoices.setItems(FXCollections.observableArrayList(
+                "Deluxe", "Supreme", "Meatzza", "Pepperoni", "Seafood")
+        );
+//        specialtyToppings.setItems(FXCollections.observableArrayList(
+//                "")
+//        );
+    }
 }
